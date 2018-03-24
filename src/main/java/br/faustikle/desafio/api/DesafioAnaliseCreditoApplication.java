5a4337a7c433d98ca752304705818b8b4e1619f0
kleber <faustikle@gmail.com>
2018-03-24 12:29:06 -0300

    Estrutura inicial

---
+++ src/main/java/br/faustikle/desafio/api/DesafioAnaliseCreditoApplication.java
@@ -0,0 +1,12 @@
+package br.faustikle.desafio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAnaliseCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAnaliseCreditoApplication.class, args);
+	}
+}
