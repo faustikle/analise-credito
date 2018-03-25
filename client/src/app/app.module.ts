1356c3141857d49469ee74af035f341ca633ccc5
kleber <faustikle@gmail.com>
2018-03-25 19:33:10 -0300

    Implementado modulo principal

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import { PrincipalComponent } from './principal/principal.component';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    ButtonModule
+  ],
+  declarations: [
+    AppComponent,
+    PrincipalComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
