e0b3db0347e6da12f9fb3f466ea855db7ac374ce
kleber <faustikle@gmail.com>
2018-03-25 18:42:07 -0300

    Adicionado filtro de cors e service docker do front

---
+++ api/src/main/java/br/faustikle/desafio/api/infrastructure/security/filter/CorsFilter.java
@@ -0,0 +1,45 @@
+package br.faustikle.desafio.api.infrastructure.security.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String origin = "http://localhost";
+
+        res.setHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if (HttpMethod.OPTIONS.toString().equals(req.getMethod()) && origin.equals(req.getHeader("Origin"))) {
+            res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+            res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+            res.setHeader("Access-Control-Max-Age", "3600");
+
+            res.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
